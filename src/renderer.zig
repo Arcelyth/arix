1151cf3beda61833dc8012de0872e63e0a8660bf
Arcelyth <arcelyth@tutamail.com>
2026-08-30 22:07:58 +0800

    remove useless previous tree_builder test

--- src/renderer.zig
+++ src/renderer.zig
@@ -9,7 +9,6 @@ pub const html5lib_tokenizer_test = @import("renderer/tests/html5lib_tokenizer_t
 pub const html5lib_tree_builder_test = @import("renderer/tests/html5lib_tree_construction_test.zig");
 pub const TestParser = @import("renderer/tests/TestParser.zig");
 pub const TreeBuilder = @import("renderer/html/tree_builder/TreeBuilder.zig");
-pub const tree_builder_test = @import("renderer/html/tree_builder/tree_builder_test.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());

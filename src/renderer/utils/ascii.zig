f78d4318b2d1446ac7031df252737a014faab3b1
Arcelyth <arcelyth@tutamail.com>
2026-08-31 14:38:44 +0800

    css: add more utils function in ascii

--- src/renderer/utils/ascii.zig
+++ src/renderer/utils/ascii.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+pub const maximum_code_point: u21 = 0x10FFFF;
+
 pub inline fn isHtmlSpace(c: u8) bool {
     return switch (c) {
         0x09, 0x0A, 0x0C, 0x0D, 0x20 => true,
@@ -68,4 +70,83 @@ pub inline fn isControlCharacter(comptime T: type, ch: T) bool {
         ch == 0x000B or
         (ch >= 0x000E and ch <= 0x001F) or
         (ch >= 0x007F and ch <= 0x009F);
+}
+
+// https://drafts.csswg.org/css-syntax/#non-ascii-ident-code-point
+pub inline fn isNonAsciiIdent(c: u21) bool {
+    return c == 0x00B7 or
+        (c >= 0x00C0 and c <= 0x00D6) or
+        (c >= 0x00D8 and c <= 0x00F6) or
+        (c >= 0x00F8 and c <= 0x037D) or
+        (c >= 0x037F and c <= 0x1FFF) or
+        c == 0x200C or
+        c == 0x200D or
+        c == 0x203F or
+        c == 0x2040 or
+        (c >= 0x2070 and c <= 0x218F) or
+        (c >= 0x2C00 and c <= 0x2FEF) or
+        (c >= 0x3001 and c <= 0xD7FF) or
+        (c >= 0xF900 and c <= 0xFDCF) or
+        (c >= 0xFDF0 and c <= 0xFFFD) or
+        c >= 0x10000;
+}
+
+// https://drafts.csswg.org/css-syntax/#ident-start-code-point
+pub inline fn isCssIdentStart(c: u21) bool {
+    return isAsciiAlpha(c) or isNonAsciiIdent(c) or c == '_';
+}
+
+// https://drafts.csswg.org/css-syntax/#ident-code-point
+pub inline fn isCssIdent(c: u21) bool {
+    return isCssIdentStart(c) or isAsciiDigit(c) or c == '-';
+}
+
+//https://drafts.csswg.org/css-syntax/#non-printable-code-point
+pub inline fn isCssNonPrintable(c: u21) bool {
+    return c <= 0x0008 or
+        c == 0x000B or
+        (c >= 0x000E and c <= 0x001F) or
+        c == 0x007F;
+}
+
+// CR and FF have already been normalized during preprocessing.
+// https://drafts.csswg.org/css-syntax/#newline
+pub inline fn isCssNewline(c: u21) bool {
+    return c == '\n';
+}
+
+// https://drafts.csswg.org/css-syntax/#whitespace
+pub inline fn isCssWhitespace(c: u21) bool {
+    return isCssNewline(c) or c == '\t' or c == ' ';
+}
+
+
+const testing = std.testing;
+
+test "CSS: code-point definitions" {
+    try testing.expect(isAsciiDigit('0'));
+    try testing.expect(isAsciiHexDigit('F'));
+    try testing.expect(!isAsciiHexDigit('g'));
+    try testing.expect(isCssIdentStart('_'));
+    try testing.expect(!isCssIdentStart('-'));
+    try testing.expect(isCssIdent('-'));
+    try testing.expect(isCssWhitespace('\n'));
+    try testing.expect(!isCssWhitespace('\r'));
+    try testing.expect(isCssNonPrintable(0x007F));
+    try testing.expect(!isCssNonPrintable(0x0080));
+}
+
+test "CSS: non-ASCII ident boundaries" {
+    try testing.expect(isNonAsciiIdent(0x00B7));
+    try testing.expect(!isNonAsciiIdent(0x00B8));
+    try testing.expect(isNonAsciiIdent(0x00C0));
+    try testing.expect(isNonAsciiIdent(0x00D6));
+    try testing.expect(!isNonAsciiIdent(0x00D7));
+    try testing.expect(isNonAsciiIdent(0x037D));
+    try testing.expect(!isNonAsciiIdent(0x037E));
+    try testing.expect(isNonAsciiIdent(0x037F));
+    try testing.expect(isNonAsciiIdent(0xFFFD));
+    try testing.expect(!isNonAsciiIdent(0xFFFE));
+    try testing.expect(isNonAsciiIdent(0x10000));
+    try testing.expect(isNonAsciiIdent(0x10FFFF));
 }
